f910fd6d4a2a4c423628256a278339619eef04b6
vinnykris <vinodkrishnam@gmail.com>
2016-01-29 21:35:48 -0800

    added translation method

--- UniversalCommunicator/app/src/main/java/com/rjvproj/universalcommunicator/DisplayMessageActivity.java
+++ UniversalCommunicator/app/src/main/java/com/rjvproj/universalcommunicator/DisplayMessageActivity.java
@@ -32,10 +32,105 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText(message);
+        textView.setText(translate(message));
 
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
         layout.addView(textView);
+
     }
+
+    public String translate (String s) {
+        // Goes through entire message and converts into morse code (character by character)
+        String str = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'A') {
+                str += ".-";
+            }
+            else if (c == 'b' || c == 'B') {
+                str += "-...";
+            }
+            else if (c == 'c' || c == 'C') {
+                str += "-.-.";
+            }
+            else if (c == 'd' || c == 'D') {
+                str += "-..";
+            }
+            else if (c == 'e' || c == 'E') {
+                str += ".";
+            }
+            else if (c == 'f' || c == 'F') {
+                str += "..-.";
+            }
+            else if (c == 'g' || c == 'G') {
+                str += "--.";
+            }
+            else if (c == 'h' || c == 'H') {
+                str += "....";
+            }
+            else if (c == 'i' || c == 'I') {
+                str += "..";
+            }
+            else if (c == 'j' || c == 'J') {
+                str += ".---";
+            }
+            else if (c == 'k' || c == 'K') {
+                str += "-.-";
+            }
+            else if (c == 'l' || c == 'L') {
+                str += ".-..";
+            }
+            else if (c == 'm' || c == 'M') {
+                str += "--";
+            }
+            else if (c == 'n' || c == 'N') {
+                str += "-.";
+            }
+            else if (c == 'o' || c == 'O') {
+                str += "---";
+            }
+            else if (c == 'p' || c == 'P') {
+                str += ".--.";
+            }
+            else if (c == 'q' || c == 'Q') {
+                str += "--.-";
+            }
+            else if (c == 'r' || c == 'R') {
+                str += ".-.";
+            }
+            else if (c == 's' || c == 'S') {
+                str += "...";
+            }
+            else if (c == 't' || c == 'T') {
+                str += "-";
+            }
+            else if (c == 'u' || c == 'U') {
+                str += "..-";
+            }
+            else if (c == 'v' || c == 'V') {
+                str += "...-";
+            }
+            else if (c == 'w' || c == 'W') {
+                str += ".--";
+            }
+            else if (c == 'x' || c == 'X') {
+                str += "-..-";
+            }
+            else if (c == 'y' || c == 'Y') {
+                str += "-.--";
+            }
+            else if (c == 'z' || c == 'Z') {
+                str += "--..";
+            }
+            else if (c == ' '){
+                str += "  ";
+            }
+            str += " ";
+        }
+        return str;
+
+    }
+
+
 
 }

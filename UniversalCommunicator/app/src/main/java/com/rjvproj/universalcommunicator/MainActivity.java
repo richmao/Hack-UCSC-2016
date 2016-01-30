64d44b1edd7754be533e8cf8f5fc1d8783081c08
Jacob Runyan <jmrunyan@ucsc.edu>
2016-01-29 22:37:30 -0800

    Menu fixed, ready to add pages/switch functionality.

--- UniversalCommunicator/app/src/main/java/com/rjvproj/universalcommunicator/MainActivity.java
+++ UniversalCommunicator/app/src/main/java/com/rjvproj/universalcommunicator/MainActivity.java
@@ -104,7 +104,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        Intent intent = new Intent(this, ActivityDisplayMessage.class);
         EditText editText = (EditText) findViewById(R.id.editText1);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
